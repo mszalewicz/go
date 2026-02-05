c10579a240a551971044fe2e6a5cb7a5904bb301
Maciej Szalewicz <m.szalewicz@gmail.com>
2026-02-05 16:13:55 +0100

    Implemented constructing buttons with padding

--- test_projects/gioui_exploratory/gui/contructs.go
+++ test_projects/gioui_exploratory/gui/contructs.go
@@ -2,9 +2,13 @@ package gui
 
 import (
 	"image"
+	"image/color"
 
 	"gioui.org/layout"
 	"gioui.org/op"
+	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
 )
 
 // SplitView is a container that show two widgets side by side.
@@ -23,7 +27,7 @@ func (s SplitView) Layout(
 		ratio = 0.5
 	}
 
-	leftsize := int( float32(gtx.Constraints.Min.X) * ratio )
+	leftsize := int(float32(gtx.Constraints.Min.X) * ratio)
 	rightsize := gtx.Constraints.Min.X - leftsize
 
 	{
@@ -42,4 +46,68 @@ func (s SplitView) Layout(
 	}
 
 	return layout.Dimensions{Size: gtx.Constraints.Max}
+}
+
+// Create buttom with custom theming and padding - used for special case buttons, e.x. "DELETE ALL" with red background
+func CreateUniqueButton(theme *material.Theme, label string, background color.NRGBA, text_color color.NRGBA, font_size int, top_inset int, bottom_inset int, left_inset int, right_inset int) (*material.ButtonStyle, layout.Widget) {
+
+	if font_size == 0 {
+		font_size = int(theme.TextSize)
+	}
+
+	// 1. Create associated clickable widget
+	var buttonWidget widget.Clickable
+
+	// 2. Initialize the material button
+	btn := material.Button(theme, &buttonWidget, label)
+	btn.Background = background
+	btn.Color = text_color
+	btn.TextSize = unit.Sp(font_size)
+
+	// 4. Return btn and associated padded layout
+	return &btn, func(gtx layout.Context) layout.Dimensions {
+
+		// 2. Define the Inset (the "outer" padding)
+		inset := layout.Inset{
+			Top:    unit.Dp(top_inset),
+			Bottom: unit.Dp(bottom_inset),
+			Left:   unit.Dp(left_inset),
+			Right:  unit.Dp(right_inset),
+		}
+
+		// 3. Return the layout wrapped in the inset
+		return inset.Layout(gtx, btn.Layout)
+	}
+}
+
+// Creates button with default theme and custom padding
+func CreateDefaultButton(theme *material.Theme, label string, font_size int, top_inset int, bottom_inset int, left_inset int, right_inset int) (*material.ButtonStyle, layout.Widget) {
+
+	if font_size == 0 {
+		font_size = int(theme.TextSize)
+	}
+
+	// 1. Create associated clickable widget
+	var buttonWidget widget.Clickable
+
+	// 2. Initialize the material button
+	btn := material.Button(theme, &buttonWidget, label)
+	btn.Background = theme.Bg
+	btn.Color = theme.Fg
+	btn.TextSize = unit.Sp(font_size)
+
+	// 5. Return button and associated padded layout
+	return &btn, func(gtx layout.Context) layout.Dimensions {
+
+		// 3. Define the Inset (the "outer" padding)
+		inset := layout.Inset{
+			Top:    unit.Dp(top_inset),
+			Bottom: unit.Dp(bottom_inset),
+			Left:   unit.Dp(left_inset),
+			Right:  unit.Dp(right_inset),
+		}
+
+		// 4. Return the layout wrapped in the inset
+		return inset.Layout(gtx, btn.Layout)
+	}
 }
--- test_projects/gioui_exploratory/main.go
+++ test_projects/gioui_exploratory/main.go
@@ -9,8 +9,10 @@ import (
 	"this/gui"
 
 	"gioui.org/app"
+	"gioui.org/font"
+	"gioui.org/layout"
 	"gioui.org/op"
-	"gioui.org/text"
+	"gioui.org/unit"
 	"gioui.org/widget/material"
 )
 
@@ -26,6 +28,22 @@ func main() {
 	app.Main()
 }
 
+var (
+	BEIGE        = color.NRGBA{R: 247, G: 239, B: 229, A: 255}
+	BLACK        = color.NRGBA{R: 0, G: 0, B: 0, A: 255}
+	BLUE         = color.NRGBA{R: 150, G: 201, B: 244, A: 255}
+	CHARCOAL     = color.NRGBA{R: 0, G: 0, B: 0, A: 150}
+	GREEN        = color.NRGBA{R: 100, G: 196, B: 166, A: 255}
+	GREY         = color.NRGBA{R: 100, G: 100, B: 100, A: 255}
+	GREY_LIGHT   = color.NRGBA{R: 235, G: 235, B: 235, A: 255}
+	ORANGE       = color.NRGBA{R: 235, G: 178, B: 10, A: 100}
+	PURPLE       = color.NRGBA{R: 161, G: 100, B: 196, A: 255}
+	PURPLE_LIGHT = color.NRGBA{R: 161, G: 100, B: 196, A: 120}
+	RED          = color.NRGBA{R: 238, G: 78, B: 78, A: 220}
+	WHITE        = color.NRGBA{R: 255, G: 255, B: 255, A: 255}
+	TEXTSIZE     = unit.Sp(30)
+)
+
 func run(window *app.Window) error {
 	theme := material.NewTheme()
 	var ops op.Ops
@@ -37,24 +55,26 @@ func run(window *app.Window) error {
 			// This graphics context is used for managing the rendering state.
 			gtx := app.NewContext(&ops, e)
 
-			// Define an large label with an appropriate text:
-			title := material.H1(theme, "Hello, Gio")
+			const buttonSize = 12
 
-			// Change the color of the label.
-			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
-			title.Color = maroon
+			theme.Bg = GREY_LIGHT
+			theme.Fg = BLACK
 
-			// Change the position of the label.
-			title.Alignment = text.Middle
+			hugeBtnInfo, hugeBtnWidget := gui.CreateDefaultButton(theme, "HUGE BUTTON!!! ", 0, 10, 10, 10, 5)
+			hugeBtnInfo.Font.Weight = font.Bold
 
-			// Draw the label to the graphics context.
-			// title.Layout(gtx)
+			openBtnInfo, openBtnWidget := gui.CreateDefaultButton(theme, "OPEN", 0, 10, 10, 5, 5)
+			openBtnInfo.Font.Weight = font.Bold
 
-			split := new(gui.SplitView)
-			split.Layout(gtx, title.Layout, title.Layout, 0.65)
+			deleteBtnInfo, deleteBtnWidget := gui.CreateDefaultButton(theme, "DELETE", 0, 10, 10, 5, 10)
+			deleteBtnInfo.Font.Weight = font.Bold
 
+			splitButtons := new(gui.SplitView)
+			splitButtonsWigdet := func(gtx layout.Context) layout.Dimensions { return splitButtons.Layout(gtx, openBtnWidget, deleteBtnWidget, 0.5) }
 
-			// Pass the drawing operations to the GPU.
+			splitEntry := new(gui.SplitView)
+			splitEntry.Layout(gtx, hugeBtnWidget, splitButtonsWigdet, 0.7)
+
 			e.Frame(gtx.Ops)
 		}
 	}

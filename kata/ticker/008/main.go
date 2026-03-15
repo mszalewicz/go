c38f46582e7c5bb645cd279e66ada8c35fcda84a
Maciej Szalewicz <m.szalewicz@gmail.com>
2026-03-15 15:54:19 +0100

    ticker kata

---
+++ kata/ticker/008/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	ticker := time.NewTicker(300 * time.Millisecond)
+
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Println(time.Now())
+		case <-ctx.Done():
+			ticker.Stop()
+			fmt.Println("ticker stopped")
+			return
+		default:
+		}
+	}
+}

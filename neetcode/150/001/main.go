915f15f1bc55841a552c784b46b88154620ef66e
Maciej Szalewicz <m.szalewicz@gmail.com>
2026-03-21 10:49:39 +0100

    neetcode 001

---
+++ neetcode/150/001/main.go
@@ -0,0 +1,32 @@
+package main
+
+
+// Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+//
+// Example 1:
+//
+// Input: nums = [1, 2, 3, 3]
+//
+// Output: true
+//
+//
+// Example 2:
+//
+// Input: nums = [1, 2, 3, 4]
+//
+// Output: false
+
+func hasDuplicate(nums []int) bool {
+    seen := make(map[int]int)
+
+    for _, number := range nums {
+        if _, ok := seen[number]; ok {
+            return true
+        }
+        seen[number] = 1
+    }
+
+    return false
+}
+
+
